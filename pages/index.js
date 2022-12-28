380d066b7f20e2f77ec9cb1d4104a4c58cbb604c
someone <someone@someone.com>
2022-12-28 16:05:58 -0500

    commit before remote git

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 import Head from 'next/head';
 import Layout from '@/components/Layout';
+import Eventitem from '@/components/Eventitem';
+import { API_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <Eventitem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
